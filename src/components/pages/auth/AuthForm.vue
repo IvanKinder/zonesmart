1bace2a983c57474575ce8df23fa207dd2eb1a70
Ivan <i.kinder@agora.ru>
2024-03-31 01:52:21 +0300

    Добавил условие для активации кнопки

--- src/components/pages/auth/AuthForm.vue
+++ src/components/pages/auth/AuthForm.vue
@@ -1,4 +1,11 @@
 <script setup lang="ts">
+import { computed, ref } from 'vue'
+
+const email = ref("")
+const password = ref("")
+const login_enabled = computed(() => email.value && password.value);
+const show_pass = ref(false)
+
 </script>
 
 <template lang="pug">
@@ -9,15 +16,29 @@ form.auth-form(class="review-form" @submit.prevent="onSubmit")
         a.no-acc-to-registry(href="#") Зарегистрироваться
     .field
         label.field-label(for="email-input") Email
-        input(id="email-input" type="text" placeholder="yourmail@mail.ru")
+        input(
+            id="email-input"
+            type="text"
+            placeholder="yourmail@mail.ru"
+            v-model="email"
+        )
     .field
-        label.field-label(for="email-input") Пароль
+        label.field-label(for="password-input") Пароль
         .field-password-input-container
-            input(id="email-input" type="password" placeholder="Ваш пароль")
-            img(src="/src/assets/show_pass.svg" alt="Показать пароль")
+            input(
+                id="password-input"
+                :type="!show_pass ? 'password' : 'text'"
+                placeholder="Ваш пароль"
+                v-model="password"
+            )
+            img(
+                :src="!show_pass ? '/src/assets/show_pass.svg' : '/src/assets/show_pass_active.svg'"
+                alt="Показать пароль"
+                @click="() => show_pass = !show_pass"
+            )
         .forget-password 
             a(href="#") Забыли пароль?
-    button.login-btn Войти
+    button.login-btn(:class="!login_enabled ? 'login-btn-disabled' : 'login-btn-enabled'") Войти
 </template>
 
 <style lang="scss" scoped>
@@ -62,8 +83,13 @@ form.auth-form(class="review-form" @submit.prevent="onSubmit")
             box-shadow: 0px 1px 2px 0px #00000040 inset;
             padding: 0 20px;
             color: black;
+            transition: box-shadow 0.5s ease-in-out;
             &::placeholder {
                 color: #999999;
+            }
+            &:active, &:focus {
+                outline: none;
+                box-shadow: 0px 0px 2px 1px rgba(0, 0, 0, 0.2509803922) inset;
             }
         }
         &-label {
@@ -93,6 +119,26 @@ form.auth-form(class="review-form" @submit.prevent="onSubmit")
         color: #CECECF;
         width: 105px;
         height: 50px;
+        transition: box-shadow 0.3s ease-in-out, color 0.3s ease-in-out;
+        &-enabled {
+            box-shadow: 0px 0px 2px 0px rgba(0, 0, 0, 0.5) inset;
+            transition: transform 0.2s ease, box-shadow 0.3s ease-in-out, color 0.3s ease-in-out;
+            color: #b9b9bd;
+            &:active {
+                transform: scale(0.95);
+            }
+            &:hover, &:focus {
+                border: 0;
+                outline: none;
+            }
+        }
+        &-disabled {
+            cursor: not-allowed;
+            &:hover, &:active, &:focus {
+                border: 0;
+                outline: none;
+            }
+        }
     }
 }
 </style>
