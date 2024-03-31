a9005d2c7f1394fa2ae45e6e574be86874b87e97
Ivan <i.kinder@agora.ru>
2024-03-31 19:23:39 +0300

    Доверстал строку действий с выделенными продуктами

--- src/components/pages/products/ProductsTable.vue
+++ src/components/pages/products/ProductsTable.vue
@@ -66,7 +66,15 @@ watch(props, () => {
         span.column-name.column-name-max-price Максимальная цена
         span.column-name.column-name-del Удалить
     .products-table-actions(v-if="ckecked_products_ids.size")
-        span test
+        span.products-table-actions-selected Выбрано {{ ckecked_products_ids.size }} из {{ store.getters.products.length }}
+        button.products-table-actions-delete 
+            img(src="/src/assets/delete_bulk.svg" alt="Удалить выделенные")
+            span Удалить выделенные
+        span.products-table-actions-for-all Для всех выделенных
+        .products-table-actions-prices.min
+            input(placeholder="₽" type="number")
+        .products-table-actions-prices.max
+            input(placeholder="₽" type="number")
     .products-table-loading(v-if="is_loading")
         img(src="/src/assets/loading.svg")
     .products-table-list(v-else v-for="product in store.getters.products")
@@ -110,12 +118,69 @@ watch(props, () => {
     height: 305px;
     overflow: scroll;
     &-actions {
+        display: grid;
+        grid-template-columns: 170px 200px 1fr 215px 255px;
+        align-items: center;
+        justify-items: center;
         position: sticky;
         top: 60px;
         z-index: 1;
         height: 60px;
         background: #e2e2e2;
-        border-bottom: 1px solid #00000033
+        border-bottom: 1px solid #00000033;
+        &-selected {
+            color: #000000;
+            font-size: 15px;
+        }
+        &-delete {
+            display: flex;
+            padding: 7px;
+            gap: 5px;
+            align-items: center;
+            height: 30px;
+            align-self: center;
+            transition: transform 0.2s ease;
+            span {
+                font-size: 15px;
+            }
+            &:hover {
+                transform: scale(1.02);
+            }
+            &:active {
+                transform: scale(0.98);
+            }
+            &:active, &:focus, &:hover {
+                outline: none;
+                border: none;
+            }
+        }
+        &-for-all {
+            font-size: 15px;
+            color: #000000;
+            justify-self: end;
+        }
+        &-prices {
+            input {
+                width: 118px;
+                height: 30px;
+                background: #F2F1F3;
+                border: 0;
+                box-shadow: 0px 1px 2px 0px #00000040 inset;
+                border-radius: 6px;
+                padding: 0 8px;
+                transition: box-shadow 0.5s ease-in-out;
+                ::placeholder {
+                    color: #000000;
+                }
+                &:active, &:focus {
+                    outline: none;
+                    box-shadow: 0px 0px 2px 1px rgba(0, 0, 0, 0.2509803922) inset;
+                }
+            }
+        }
+        &-prices.max {
+            justify-self: start;
+        }
     }
     .checkbox-btn {
         position: relative;
